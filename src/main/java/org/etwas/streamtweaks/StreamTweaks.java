b5a8405593a8e7cade09c334b6e39f7774d9c6e9
aktnb <70847394+aktnb@users.noreply.github.com>
2025-09-13 19:59:38 +0900

    initialize Stream Tweaks mod with basic structure and configuration files

---
+++ src/main/java/org/etwas/streamtweaks/StreamTweaks.java
@@ -0,0 +1,10 @@
+package org.etwas.streamtweaks;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StreamTweaks implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/org/etwas/streamtweaks/client/StreamTweaksClient.java
@@ -0,0 +1,10 @@
+package org.etwas.streamtweaks.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class StreamTweaksClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
